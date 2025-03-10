9ed909ba0dcc6b23cefc0090beac75bbdf8a7700
yuehuhu <34811426+yuehuhu@users.noreply.github.com>
2025-03-10 15:26:05 +0800

    Create test_math_utils.cpp

---
+++ test/test_math_utils.cpp
@@ -0,0 +1,21 @@
+#include "math_utils.h"
+#include <cassert>
+
+void test_add() {
+    assert(add(3, 4) == 7);
+    assert(add(-1, 1) == 0);
+    assert(add(-3, -4) == -7);
+}
+
+void test_subtract() {
+    assert(subtract(5, 3) == 2);
+    assert(subtract(-1, 1) == -2);
+    assert(subtract(-3, -4) == 1);
+}
+
+int main() {
+    test_add();
+    test_subtract();
+    std::cout << "All tests passed!" << std::endl;
+    return 0;
+}
