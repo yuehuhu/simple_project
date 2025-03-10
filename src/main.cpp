52cc2608bb1a8b9f8385c8a4d1506899b8af6d87
yuehuhu <34811426+yuehuhu@users.noreply.github.com>
2025-03-10 15:10:11 +0800

    Create main.cpp

---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "math_utils.h"
+
+int main() {
+    int a = 5;
+    int b = 3;
+
+    std::cout << "Add: " << add(a, b) << std::endl;
+    std::cout << "Subtract: " << subtract(a, b) << std::endl;
+
+    return 0;
+}
