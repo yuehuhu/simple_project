0ac1e3c43d5c846ab769dcd76632109f03c594e6
栎竹 <taowenhui.twh@alibaba-inc.com>
2025-04-03 10:11:25 +0800

    更新单元测试

--- src/math_utils.cpp
+++ src/math_utils.cpp
@@ -7,3 +7,7 @@ int add(int a, int b) {
 int subtract(int a, int b) {
     return a - b;
 }
+
+int subtracttt(int a, int b) {
+    return a - b + b;
+}
--- test/test_math_utils.cpp
+++ test/test_math_utils.cpp
@@ -11,5 +11,5 @@ TEST_CASE("add is ok", "[math_utils]") {
 TEST_CASE("subtract is ok", "[math_utils]") {
   REQUIRE(subtract(5, 3) == 2);
   REQUIRE(subtract(-1, 1) == -2);
-  REQUIRE(subtract(-3, -4) == 1);
+  REQUIRE(subtract(-3, -5) == 2);
 }
