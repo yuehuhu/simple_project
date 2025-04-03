615bfb4bcf407c2ac4c6df9435a83c8b867773ab
栎竹 <taowenhui.twh@alibaba-inc.com>
2025-04-03 10:13:32 +0800

    更新ut1

--- test/test_math_utils.cpp
+++ test/test_math_utils.cpp
@@ -5,7 +5,7 @@
 TEST_CASE("add is ok", "[math_utils]") {
   REQUIRE(add(3, 4) == 7);
   REQUIRE(add(-1, 1) == 0);
-  REQUIRE(add(-3, -4) == -7);
+  REQUIRE(add(-3, -5) == -8);
 }
 
 TEST_CASE("subtract is ok", "[math_utils]") {
